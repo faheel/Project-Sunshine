ca1ebfc996b8b4cc9c769eff6c75d471b1f12bc0
Faheel Ahmad <faheel@live.in>
2017-02-03 21:58:20 +0530

    Hide API key, update versions for gradle, SDK, and build-tools

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -246,14 +246,13 @@ public class ForecastFragment extends Fragment {
 				final String UNITS_PARAM = "units";
 				final String DAYS_PARAM = "cnt";
 				final String APPID_PARAM = "appid";
-				final String OPEN_WEATHER_MAP_API_KEY = "***REMOVED***";
 
 				Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
 						.appendQueryParameter(QUERY_PARAM, params[0])
 						.appendQueryParameter(FORMAT_PARAM, format)
 						.appendQueryParameter(UNITS_PARAM, units)
 						.appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
-						.appendQueryParameter(APPID_PARAM, OPEN_WEATHER_MAP_API_KEY)
+						.appendQueryParameter(APPID_PARAM, BuildConfig.OPEN_WEATHER_MAP_API_KEY)
 						.build();
 
 				URL url = new URL(builtUri.toString());
--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
